d425d830b4ff54c5577b699190f8edae1135222e
vadimcimbalyk <vadymonua@gmail.com>
2022-09-22 13:22:50 +0300

    fix bugs

--- app/js/script.js
+++ app/js/script.js
@@ -15,7 +15,7 @@ $(".form").validate({
             email: "Your email address must be in the format of name@domain.com"
         },
         password: {
-            required: "Please enter your password",
+            required: "Enter password",
             minlength: "minimal length 8",
             maxlength: "max length 16"
         }
@@ -23,9 +23,9 @@ $(".form").validate({
     focusInvalid: true,
     errorClass: "input-error",
     submitHandler: function () {
-        console.log(document.querySelector(".form-row__input-email").value)
-        console.log(document.querySelector(".form-row__input-password").value)
-        document.querySelector(".form-row__input-email").value = "";
-        document.querySelector(".form-row__input-password").value = "";
+        console.log(document.querySelector(".form__input-email").value)
+        console.log(document.querySelector(".form__input-password").value)
+        document.querySelector(".form__input-email").value = "";
+        document.querySelector(".form__input-password").value = "";
     }
 })
